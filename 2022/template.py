540f6478a91b0d1a5c8e00db73734817339f9b3e
Liebhold <liebhold@kuennecke.com>
2022-12-08 10:10:50 +0100

    updated template

--- 2022/template.py
+++ 2022/template.py
@@ -2,12 +2,14 @@ import copy, json, math, regex
 
 lines = [line.strip("\n") for line in open("input/day8.txt").readlines()]
 
-map_val = [[]]
-for x, _ in enumerate(lines[0]):
-  map_val.append([])
-  for y, char in enumerate(lines[x]):
-    map_val[x].append(int(char))
-    assert map_val[x][y] == int(char), "mapping failed!"
+def map_data(lines):
+  map_val = [[]]
+  for x, _ in enumerate(lines[0]):
+    map_val.append([])
+    for y, char in enumerate(lines[x]):
+      map_val[x].append(int(char))
+      assert map_val[x][y] == int(char), "mapping failed!"
+  return map_val
 
 
   # caps = regex.search(r"((?P<com>cd|dir|\d+|ls)\s?(?P<tar>[\w/\.]+)?)", line)
