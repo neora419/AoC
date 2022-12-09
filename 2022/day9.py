dcd840e03f3454de532849e79435d05b68869b97
Liebhold <liebhold@kuennecke.com>
2022-12-09 07:20:21 +0100

    day 9

---
+++ 2022/day9.py
@@ -0,0 +1,80 @@
+import copy, json, math, regex
+
+lines = [line.strip("\n") for line in open("input/day9.txt").readlines()]
+
+def map_data(lines):
+  map_val = [[]]
+  for x, _ in enumerate(lines[0]):
+    map_val.append([])
+    for y, char in enumerate(lines[x]):
+      map_val[x].append(int(char))
+      assert map_val[x][y] == int(char), "mapping failed!"
+  return map_val
+
+head = [0, 0]
+tail = [0, 0]
+knots = [[0, 0],]
+for _ in range(9):
+  knots.append([0, 0])
+print(len(knots))
+history = {}
+history["0 0"] = 1
+for line in lines:
+  com, val = line.split()
+  for step in range(int(val)):
+    if com == "U":
+      knots[0][1] += int(int(val) / abs(int(val)))
+    if com == "D":
+      knots[0][1] -= int(int(val) / abs(int(val)))
+    if com == "R":
+      knots[0][0] += int(int(val) / abs(int(val)))
+    if com == "L":
+      knots[0][0] -= int(int(val) / abs(int(val)))
+    for i, knot in enumerate(knots):
+      if i == 0: continue
+      diff_x = abs(knots[i - 1][0] - knots[i][0])
+      diff_y = abs(knots[i - 1][1] - knots[i][1])
+      # print("head: ", head)
+
+      # print(diff_x,diff_y)
+      if (diff_x > 0 and diff_y > 0) and (diff_x > 1 or diff_y > 1):
+        if knots[i - 1][0] > knots[i][0] and knots[i - 1][1] > knots[i][1]:
+          knots[i][0] += 1
+          knots[i][1] += 1 
+        elif knots[i - 1][0] < knots[i][0] and knots[i - 1][1] < knots[i][1]:
+          knots[i][0] -= 1
+          knots[i][1] -= 1
+        elif knots[i - 1][1] > knots[i][1] and knots[i - 1][0] < knots[i][0]:
+          knots[i][0] -= 1
+          knots[i][1] += 1
+        elif knots[i - 1][1] < knots[i][1] and knots[i - 1][0] > knots[i][0]:
+          knots[i][0] += 1
+          knots[i][1] -= 1
+        # print("tail: ",tail)
+        diff_x = abs(knots[i - 1][0] - knots[i][0])
+        diff_y = abs(knots[i - 1][1] - knots[i][1])
+
+
+      if diff_x > 1 or diff_y > 1:
+        if diff_x == 0: pass
+        elif knots[i - 1][0] > knots[i][0]:
+          knots[i][0] += 1
+        elif knots[i - 1][0] < knots[i][0]:
+          knots[i][0] -= 1
+        if diff_y == 0: pass
+        elif knots[i - 1][1] > knots[i][1]:
+          knots[i][1] += 1
+        elif knots[i - 1][1] < knots[i][1]:
+          knots[i][1] -= 1
+      if i == len(knots) - 1:
+        history[f"{knots[i][0]} {knots[i][1]}"] = 1
+      # print("tail: ",tail)
+print(history.keys())
+print(len(history.keys()))
+
+
+
+
+  # caps = regex.search(r"((?P<com>cd|dir|\d+|ls)\s?(?P<tar>[\w/\.]+)?)", line)
+
+  # if "ls" in caps.captures("com"): continue
